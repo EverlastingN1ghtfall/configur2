82c9d918ca7927a0e3ed100746785fc042ebb353
_D1zast3R_ <petru00000@mail.ru>
2024-11-07 20:46:22 +0300

    flowchart of hashes

--- configur2/configur2.py
+++ configur2/configur2.py
@@ -86,19 +86,52 @@ if __name__ == '__main__':
 '''
 
 import os
-import subprocess
+import tomllib
 
 def get_commits(mas: list):
     hashes = []
-    for i in mas:
-        if i.split(' ')[0] == "commit":
-            hashes.append(i.split(' ')[1])
+    for i in range(len(mas)):
+        if mas[i].split(' ')[0] == "commit":
+            hashes.append({"hash": mas[i].split(' ')[1]})
+            if mas[i+1].split(':')[0] == "Merge":
+                hashes[-1]["type"] = "merge"
+                hashes[-1]["merging"] = [mas[i+1].split(' ')[1], mas[i+1].split(' ')[2]]
+            else:
+                hashes[-1]["type"] = "commit"
+                hashes[-1]["merging"] = []
+
     return hashes
 
-text_raw_nomerge = os.popen("git -C C:/Users/artem/fireshare log --no-merges").read()
-text_split_nomerge = text_raw_nomerge.splitlines()
-commit_hashes_nomerge = get_commits(text_split_nomerge)
-text_raw_merge = os.popen("git -C C:/Users/artem/fireshare log --merges").read()
-text_split_merge = text_raw_merge.splitlines()
-commit_hashes_merge = get_commits(text_split_merge)
-print(len(commit_hashes_nomerge), len(commit_hashes_merge))
+
+mmd_file = "temp.mmd"
+f = open(mmd_file, 'w')
+f.write("flowchart TB\n")
+text_raw_full = os.popen("git -C C:/Users/artem/fireshare --no-pager log").read()
+text_split_full = text_raw_full.splitlines()
+commits = get_commits(text_split_full)
+commits.reverse()
+for i in range(len(commits) - 1):
+    if commits[i+1]["type"] == "commit":
+        p1 = commits[i]["hash"][:7]
+        p2 = commits[i+1]["hash"][:7]
+        f.write(f"\t{p1} --> {p2}\n")
+
+for i in range(len(commits)):
+    if commits[i]["type"] == "merge":
+        p1 = commits[i]["merging"][0]
+        p2 = commits[i]["merging"][1]
+        p3 = commits[i]["hash"][:7]
+        f.write(f"\t{p1} --> {p3}\n")
+        f.write(f"\t{p2} --> {p3}\n")
+
+f.close()
+out = os.popen("mmdc -c C:/Users/artem/source/repos/configur2/configur2/init.json -i C:/Users/artem/source/repos/configur2/configur2/temp.mmd -o C:/Users/artem/source/repos/configur2/configur2/output.png").read()
+print(out)
+#mmdc -i 'input.mmd' -o 'output.png'
+
+# text_raw_nomerge = os.popen("git -C C:/Users/artem/fireshare --no-pager log --no-merges").read()
+# text_split_nomerge = text_raw_nomerge.splitlines()
+# commit_hashes_nomerge = get_commits(text_split_nomerge)
+# text_raw_merge = os.popen("git -C C:/Users/artem/fireshare --no-pager log --merges").read()
+# text_split_merge = text_raw_merge.splitlines()
+# commit_hashes_merge = get_commits(text_split_merge)
